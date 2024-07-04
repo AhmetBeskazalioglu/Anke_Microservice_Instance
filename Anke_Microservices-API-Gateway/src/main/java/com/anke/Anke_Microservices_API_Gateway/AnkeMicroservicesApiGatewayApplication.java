cd9d7f1bccfdc7c12360d633f59a625d05dcb7d6
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-07-04 19:52:59 +0300

    config and api gateway application.properties

--- Anke_Microservices-API-Gateway/src/main/java/com/anke/Anke_Microservices_API_Gateway/AnkeMicroservicesApiGatewayApplication.java
+++ Anke_Microservices-API-Gateway/src/main/java/com/anke/Anke_Microservices_API_Gateway/AnkeMicroservicesApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.anke.Anke_Microservices_API_Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AnkeMicroservicesApiGatewayApplication {
 
--- Anke_Microservices-Config-Server/src/main/java/com/anke/Anke_Microservices_Config_Server/AnkeMicroservicesConfigServerApplication.java
+++ Anke_Microservices-Config-Server/src/main/java/com/anke/Anke_Microservices_Config_Server/AnkeMicroservicesConfigServerApplication.java
@@ -2,7 +2,11 @@ package com.anke.Anke_Microservices_Config_Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // Bu uygulamanın bir Config sunucusu olduğunu belirtir.
+@EnableDiscoveryClient // Bu uygulamanın bir Eureka sunucusu olduğunu belirtir.
 @SpringBootApplication
 public class AnkeMicroservicesConfigServerApplication {
 
