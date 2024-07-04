f06d7fe53dc3eb625cad111d9f84e3acb8c264d7
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-07-04 19:32:27 +0300

    API-Gateway

---
+++ Anke_Microservices-API-Gateway/src/main/java/com/anke/Anke_Microservices_API_Gateway/AnkeMicroservicesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.anke.Anke_Microservices_API_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkeMicroservicesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkeMicroservicesApiGatewayApplication.class, args);
+	}
+
+}
---
+++ Anke_Microservices-API-Gateway/src/test/java/com/anke/Anke_Microservices_API_Gateway/AnkeMicroservicesApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anke.Anke_Microservices_API_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkeMicroservicesApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
