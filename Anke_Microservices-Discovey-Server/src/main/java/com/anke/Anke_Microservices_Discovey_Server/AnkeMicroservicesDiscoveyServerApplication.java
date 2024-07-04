925693806d032d50a06c1626e72f4e655b0b1101
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-07-04 19:31:30 +0300

    Discovery-Server

---
+++ Anke_Microservices-Discovey-Server/src/main/java/com/anke/Anke_Microservices_Discovey_Server/AnkeMicroservicesDiscoveyServerApplication.java
@@ -0,0 +1,13 @@
+package com.anke.Anke_Microservices_Discovey_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkeMicroservicesDiscoveyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkeMicroservicesDiscoveyServerApplication.class, args);
+	}
+
+}
---
+++ Anke_Microservices-Discovey-Server/src/test/java/com/anke/Anke_Microservices_Discovey_Server/AnkeMicroservicesDiscoveyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anke.Anke_Microservices_Discovey_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkeMicroservicesDiscoveyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
