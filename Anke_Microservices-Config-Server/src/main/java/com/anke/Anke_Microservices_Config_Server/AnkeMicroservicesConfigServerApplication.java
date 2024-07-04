1e558fc506eca5e4cc3ddf3b3ed65f7adf38ead4
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-07-04 19:32:06 +0300

    Config-Server

---
+++ Anke_Microservices-Config-Server/src/main/java/com/anke/Anke_Microservices_Config_Server/AnkeMicroservicesConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.anke.Anke_Microservices_Config_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkeMicroservicesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkeMicroservicesConfigServerApplication.class, args);
+	}
+
+}
---
+++ Anke_Microservices-Config-Server/src/test/java/com/anke/Anke_Microservices_Config_Server/AnkeMicroservicesConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anke.Anke_Microservices_Config_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkeMicroservicesConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
