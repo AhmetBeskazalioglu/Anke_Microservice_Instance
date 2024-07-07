cf137ca754c465380ce63670bff719f55f4b0d8e
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-07-07 23:55:04 +0300

    shoppincart-service is created

---
+++ Anke_Microservices-ShoppingCart-Service/src/test/java/com/anke/Anke_Microservices_Product_Service/AnkeMicroservicesProductServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.anke.Anke_Microservices_Product_Service;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AnkeMicroservicesProductServiceApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
